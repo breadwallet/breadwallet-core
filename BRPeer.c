07b3eaf18d2fdd589a341bef1c0eeb7e00d5b174
Aaron Voisine <voisine@gmail.com>
2017-08-17 15:41:10 -0700

    print raw tx when publishing

--- BRPeer.c
+++ BRPeer.c
@@ -542,7 +542,13 @@ static int _BRPeerAcceptGetdataMessage(BRPeer *peer, const uint8_t *msg, size_t 
                     if (tx && BRTransactionSize(tx) < TX_MAX_SIZE) {
                         uint8_t buf[BRTransactionSerialize(tx, NULL, 0)];
                         size_t bufLen = BRTransactionSerialize(tx, buf, sizeof(buf));
+                        char txHex[bufLen*2 + 1];
                         
+                        for (size_t j = 0; j < bufLen; j++) {
+                            sprintf(&txHex[j*2], "%02x", buf[j]);
+                        }
+                        
+                        peer_log(peer, "publishing tx: %s", txHex);
                         BRPeerSendMessage(peer, buf, bufLen, MSG_TX);
                         break;
                     }
