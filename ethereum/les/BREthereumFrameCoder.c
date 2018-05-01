e26cc8ce1f969e1820d5da9197b8e46f43b8097d
Lamont Samuels <lamont.samuels@breadwallet.com>
2018-05-01 08:41:59 -0500

    Core-10: Fix compile errors and Add authentication and decryption

--- ethereum/les/BREthereumFrameCoder.c
+++ ethereum/les/BREthereumFrameCoder.c
@@ -23,6 +23,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+#include <stdlib.h>
 #include "aes.h"
 #include "sha3.h"
 #include "BRCrypto.h"
@@ -30,6 +31,9 @@
 #include "BREthereumFrameCoder.h"
 #include "BREthereumLESBase.h"
 #include "BRRlpCoder.h"
+#include "BRArray.h"
+
+#define UINT256_SIZE 32
 
 /**
  *
@@ -39,6 +43,9 @@ typedef struct {
 
     //Encryption for frame
     struct aes256_ctx frameEncrypt;
+    
+    //Decryption for frame
+    struct aes256_ctx frameDecrypt;
     
     //Encryption for Mac
     struct aes256_ctx macEncrypt;
@@ -54,20 +61,109 @@ typedef struct {
 //
 // Private Functions
 //
-static BREthereumBoolean _initFrameCoder(BREthereumFrameCoderContext* ctx,
-                     UInt512* remoteEphemeral,
-                     UInt256* remoteNonce,
-                     BRKey* ecdheLocal,
-                     UInt256* localNonce,
-                     uint8_t* aukCipher,
-                     size_t aukCipherLen,
-                     uint8_t* authCiper,
-                     size_t authCipherLen,
-                     BREthereumBoolean didOriginate)
-
+void _egressDigest(BREthereumFrameCoderContext* ctx, UInt128 * digest)
 {
+
+    struct sha3_256_ctx curEgressMacH;
+    memcpy(&curEgressMacH, &ctx->egressMac, sizeof(struct sha3_256_ctx));
+    sha3_256_digest(&curEgressMacH, sizeof(digest->u8), digest->u8);
+}
+void _ingressDigest(BREthereumFrameCoderContext* ctx, UInt128 * digest)
+{
+    struct sha3_256_ctx curIngressMacH;
+    memcpy(&curIngressMacH, &ctx->ingressMac, sizeof(struct sha3_256_ctx));
+    sha3_256_digest(&curIngressMacH, sizeof(digest->u8), digest->u8);
+}
+void _updateMac(BREthereumFrameCoderContext* ctx, struct sha3_256_ctx* mac, uint8_t* sData, size_t sDataSize) {
+
+    //Peform check for sData size is h1238 _seed.size() && _seed.size() != h128::size)
+    struct sha3_256_ctx prevDigest;
+    memcpy(&prevDigest, mac, sizeof(struct sha3_256_ctx));
+    UInt128 encDigest;
+    
+    sha3_256_digest(&prevDigest, 16, encDigest.u8);
+    
+    UInt128 pDigest;
+    
+    memcpy(&pDigest.u8, &encDigest.u8, 16);
+    
+    aes256_encrypt(&ctx->macEncrypt, 16, encDigest.u8, encDigest.u8);
+
+    UInt128 xOrDigest;
+    
+    if (sDataSize){
+        ethereumXORBytes(encDigest.u8, sData, xOrDigest.u8, 128);
+    }
+    else{
+        ethereumXORBytes(encDigest.u8, pDigest.u8, xOrDigest.u8, 128);
+    }
+
+    sha3_256_update(mac, sizeof(encDigest.u8), encDigest.u8);
+    
+}
+void _writeFrame(BREthereumFrameCoderContext* ctx, BRRlpData * headerData, uint8_t* payload, size_t payloadSize, uint8_t** oBytes, size_t * oBytesSize)
+{
+    // TODO: SECURITY check header values && header <= 16 bytes
+    size_t uint256_size = 32;
+    uint8_t headerMac[uint256_size];
+    memcpy(headerMac, headerData->bytes, headerData->bytesCount);
+    
+    aes256_encrypt(&ctx->frameEncrypt, 16, headerMac, headerMac);
+    
+    _updateMac(ctx, &ctx->egressMac, headerMac, 16);
+    UInt128 egressDigest;
+    
+    _egressDigest(ctx, &egressDigest);
+    
+    
+    memcpy(&headerMac[16], egressDigest.u8, sizeof(egressDigest.u8));
+    
+    uint32_t padding = (16 - (payloadSize % 16)) % 16;
+    
+
+    size_t oBytesPtrSize = 32 + payloadSize + padding + 16;
+    uint8_t* oBytesPtr = (uint8_t *)calloc(oBytesPtrSize, sizeof(uint8_t));
+    
+    memcpy(oBytesPtr, headerMac, sizeof(headerMac));
+    
+    aes256_encrypt(&ctx->frameEncrypt, payloadSize, &oBytesPtr[32], payload);
+
+    if (padding) {
+        aes256_encrypt(&ctx->frameEncrypt, padding, &oBytesPtr[32 + payloadSize], &oBytesPtr[32 + payloadSize]);
+    }
+    
+    
+    sha3_256_update(&ctx->egressMac, payloadSize + padding, &oBytesPtr[32]);
+    _updateMac(ctx, &ctx->egressMac, NULL, 0);
+    
+    UInt128 egressDigestFrame;
+    _egressDigest(ctx, &egressDigestFrame);
+    memcpy(&oBytesPtr[32 + payloadSize + padding], egressDigestFrame.u8, sizeof(egressDigestFrame.u8));
+    
+    *oBytes = oBytesPtr;
+    *oBytesSize = oBytesPtrSize;
+}
+//
+// Public Functions
+//
+BREthereumFrameCoder ethereumFrameCoderCreate(void) {
+    
+    BREthereumFrameCoderContext * ctx = (BREthereumFrameCoderContext*) calloc (1, sizeof(*ctx));
+    return (BREthereumFrameCoder)ctx;
+}
+BREthereumBoolean ethereumFrameCoderInit(BREthereumFrameCoder fCoder,
+                            UInt512* remoteEphemeral,
+                            UInt256* remoteNonce,
+                            BRKey* ecdheLocal,
+                            UInt256* localNonce,
+                            uint8_t* aukCipher,
+                            size_t aukCipherLen,
+                            uint8_t* authCiper,
+                            size_t authCipherLen,
+                            BREthereumBoolean didOriginate) {
+    
+    BREthereumFrameCoderContext * ctx = (BREthereumFrameCoderContext*) fCoder;
     uint8_t keyMaterial[64];
-    uint8_t keyMaterialBuff[64];
     size_t uint256_size = 32;
     
     // shared-secret = sha3(ecdhe-shared-secret || sha3(nonce || initiator-nonce))
@@ -85,7 +181,7 @@ static BREthereumBoolean _initFrameCoder(BREthereumFrameCoderContext* ctx,
     memcpy(nonceMaterial.u8, lNonce->u8, uint256_size);
 
 
-    Uint256* rNonce = ETHEREUM_BOOLEAN_IS_TRUE(didOriginate)  ? localNonce : remoteNonce;
+    UInt256* rNonce = ETHEREUM_BOOLEAN_IS_TRUE(didOriginate)  ? localNonce : remoteNonce;
     memcpy(&nonceMaterial.u8[sizeof(lNonce->u8)], rNonce->u8, uint256_size);
 
     
@@ -101,7 +197,7 @@ static BREthereumBoolean _initFrameCoder(BREthereumFrameCoderContext* ctx,
     BRKeccak256(&keyMaterial[uint256_size], &keyMaterial[uint256_size], uint256_size);
     
     aes256_set_encrypt_key(&ctx->frameEncrypt, &keyMaterial[uint256_size]);
-    aes256_set_decrypt_key(&ctx->frameEncrypt, &keyMaterial[uint256_size]);
+    aes256_set_encrypt_key(&ctx->frameDecrypt, &keyMaterial[uint256_size]);
     
 
     // mac-secret = sha3(ecdhe-shared-secret || aes-secret)
@@ -114,38 +210,40 @@ static BREthereumBoolean _initFrameCoder(BREthereumFrameCoderContext* ctx,
     // Recipient egress-mac: sha3(mac-secret^initiator-nonce || auth-sent-ack)
     //           ingress-mac: sha3(mac-secret^recipient-nonce || auth-recvd-init)
     UInt256 xORMacRemoteNonce;
-    ethereumXORBytes(&keyMaterial[uint256_size], remoteNonce, xORMacRemoteNonce,uint256_size);
-    memcpy(keyMaterial, xORMacRemoteNonce, uint256_size);
-    uint8_t* egressCipher, ingressCipher;
-    size_t egreesCipherLen, ingressCipherLen;
+    ethereumXORBytes(&keyMaterial[uint256_size], remoteNonce->u8, xORMacRemoteNonce.u8, uint256_size);
+    memcpy(keyMaterial, xORMacRemoteNonce.u8, uint256_size);
+    uint8_t* egressCipher, *ingressCipher;
+    size_t egressCipherLen, ingressCipherLen;
     
     if(didOriginate)
     {
         egressCipher = aukCipher;
-        egreesCipherLen = aukCipherLen;
+        egressCipherLen = aukCipherLen;
         ingressCipher = aukCipher;
         ingressCipherLen = aukCipherLen;
     }
     else
     {
         egressCipher = authCiper;
-        egreesCipherLen = authCipherLen;
+        egressCipherLen = authCipherLen;
         ingressCipher = authCiper;
         ingressCipherLen = authCipherLen;
     }
     
-    char* gressBytes;
+    uint8_t* gressBytes;
     size_t egressBytesLen = uint256_size + egressCipherLen;
+    
     
     array_new(gressBytes, egressBytesLen);
     array_add_array(gressBytes, keyMaterial, uint256_size);
-    array_insert_array(gressBytes, uint256_size, egressCipher, egreesCipherLen);
+    array_insert_array(gressBytes, uint256_size, egressCipher, egressCipherLen);
     sha3_256_init(&ctx->egressMac);
+    
     sha3_256_update(&ctx->egressMac, egressBytesLen, gressBytes);
     
     // recover mac-secret by re-xoring remoteNonce
     UInt256 xOrMacSecret;
-    ethereumXORBytes(xORMacRemoteNonce.u8, localNonce->u8, xOrMacSecret,uint256_size);
+    ethereumXORBytes(xORMacRemoteNonce.u8, localNonce->u8, xOrMacSecret.u8, uint256_size);
     size_t ingressBytesLen = uint256_size + egressCipherLen;
 
     array_set_capacity(gressBytes, ingressBytesLen);
@@ -155,127 +253,73 @@ static BREthereumBoolean _initFrameCoder(BREthereumFrameCoderContext* ctx,
     sha3_256_update(&ctx->ingressMac, ingressBytesLen, gressBytes);
 
     array_free(gressBytes);
+    
+    return ETHEREUM_BOOLEAN_TRUE; 
 }
-void _egressDigest(BREthereumFrameCoder fCoder, UInt128 * digest)
-{
+void ethereumFrameCoderFree(BREthereumFrameCoder coder) {
 
-    struct sha3_256_ctx curEgressMacH;
-    memcpy(&curEgressMacH, fCoder->egressMac, sizeof(struct sha3_256_ctx));
-    sha3_256_digest(&curEgressMacH, sizeof(digest.u8), digest.u8);
-}
-void _ingressDigest(BREthereumFrameCoder fCoder, UInt128 * digest)
-{
-
-    struct sha3_256_ctx curIngressMacH;
-    memcpy(&curIngressMacH, fCoder->ingressMac, sizeof(struct sha3_256_ctx));
-    sha3_256_digest(&curIngressMacH, sizeof(digest.u8), digest.u8);
-}
-void _updateMac(BREthereumFrameCoder fCoder, struct sha3_256_ctx* mac, uint8_t* sData, size_t sDataSize) {
-
-    //Peform check for sData size is h1238 _seed.size() && _seed.size() != h128::size)
-    struct sha3_256_ctx prevDigest;
-    memcpy(&prevDigest, mac, sizeof(struct sha3_256_ctx));
-    UInt128 encDigest;
-    
-    sha3_256_digest(&prevDigest, encDigest.u8,16);
-    
-    UInt128 pDigest;
-    
-    memcpy(&pDigest.u8, &encDigest.u8, 16);
-    
-    aes256_encrypt(fCoder->macEncrypt, 16, encDigest.u8, encDigest.u8);
-
-    UInt128 xOrDigest;
-    
-    if (sDataSize){
-        ethereumXORBytes(encDigest.u8, sData, xOrDigest.u8, 128);
-    }
-    else{
-        ethereumXORBytes(encDigest.u8, pDigest.u8, xOrDigest.u8, 128);
-    }
-
-    sha3_256_update(mac, encDigest.u8, sizeof(encDigest.u8));
-    
-}
-void _writeFrame(BREthereumFrameCoder fCoder, BRRlpData * headerData, uint8_t* payload, size_t payloadSize, uint8_t** oBytes, size_t * oBytesSize)
-{
-    // TODO: SECURITY check header values && header <= 16 bytes
-    size_t uint256_size = 32;
-    uint8_t headerMac[uint256_size];
-    memcpy(headerMac, headerData->bytes, headerData->bytesCount);
-    
-    aes256_encrypt(fCoder->frameEncrypt, 16, headerMac, headerMac);
-    
-    _updateMac(fCoder, &fCoder->egressMac, headerMac, 16);
-    UInt128 egressDigest;
-    
-    _egressDigest(fCoder, &egressDigest);
-    
-    
-    memcpy(&headerMac[16], egressDigest.u8, sizeof(egressDigest.u8));
-    
-    uint32_t padding = (16 - (payloadSize % 16)) % 16;
-    
-
-    oBytesSize = 32 + payloadSize + padding + 16;
-    oBytes = (uint8_t *)calloc(oBytesSize, sizeof(uint8_t));
-    
-    memcpy(oBytes, headerMac, sizeof(headerMac));
-    
-    aes256_encrypt(fCoder->frameEncrypt, payloadSize, &oBytes[32], payload);
-
-    bytesRef paddingRef(o_bytes.data() + 32 + _payload.size(), padding);
-    
-    
-    if (padding) {
-        aes256_encrypt(fCoder->frameEncrypt, padding, &oBytes[32 + _payload.size()], &oBytes[32 + _payload.size()]);
-    }
-    
-    
-    sha3_256_update(&ctx->egressMac, &[oBytes + 32], payloadSize + padding);
-    _updateMac(fCoder, &fCoder->egressMac, NULL, 0);
-    m_impl->egressMac.Update(_cipher.data(), _cipher.size());
-    
-    
-    UInt128 egressDigestFrame;
-    _egressDigest(fCoder, &egressDigestFrame);
-    memcpy(&oBytes[32 + payloadSize + padding], egressDigestFrame.u8, sizeof(egressDigestFrame.u8));
-}
-//
-// Public Functions
-//
-BREthereumFrameCoder ethereumFrameCoderCreate(UInt512* remoteEphemeral,
-                                              UInt256* remoteNonce,
-                                              BRKey* ecdheLocal,
-                                              UInt256* localNonce,
-                                              uint8_t* aukCipher,
-                                              size_t aukCipherLen,
-                                              uint8_t* authCiper,
-                                              size_t authCipherLen,
-                                              BREthereumBoolean didOriginate) {
-    
-    BREthereumPacketFramerContext * ctx = (BREthereumFrameCoderContext) calloc (1, sizeof(*ctx));
-    _initFrameCoder(ctx,remoteEphemeral,remoteNonce,ecdheLocal,localNonce,aukCipher,aukCipherLen,authCipherLen,authCipherLen,didOriginate);
-    return ctx;
+    BREthereumFrameCoderContext* ctx = (BREthereumFrameCoderContext*)coder;
+    free(ctx);
 }
 void ethereumFrameCoderWrite(BREthereumFrameCoder fCoder, uint8_t msgId,  uint8_t* payload, size_t payloadSize, uint8_t** oBytes, size_t * oBytesSize) {
+
+    BREthereumFrameCoderContext* ctx = (BREthereumFrameCoderContext*) fCoder;
 
     BRRlpCoder coder = rlpCoderCreate();
 
     BRRlpItem headerItem;
     
-    uint32_t frameSize =  sizeof(msgId) + payloadSize;
+    uint32_t frameSize =  (uint32_t)(sizeof(msgId) + payloadSize);
     
-    uint8_t header[4] = {(uint8_t)((frameSize >> 16) & 0xff), ((uint8_t))((frameSize >> 8) & 0xff), (uint8_t)(frameSize & 0xff), msgId};
+    uint8_t header[4] = {(uint8_t)((frameSize >> 16) & 0xff), (uint8_t)((frameSize >> 8) & 0xff), (uint8_t)(frameSize & 0xff), msgId};
     
     headerItem = rlpEncodeItemBytes(coder, header, sizeof(header));
     
     BRRlpData headerData;
 
-    rlpDataExtract(coder, headerItem, &headerData);
+    rlpDataExtract(coder, headerItem, &headerData.bytes, &headerData.bytesCount);
 
-    _writeFrame(fCoder, &headerData, payload, payloadSize, oBytes, oBytesSize);
+    _writeFrame(ctx, &headerData, payload, payloadSize, oBytes, oBytesSize);
+    
+    rlpCoderRelease(coder);
 }
+BREthereumBoolean ethereumFrameCoderDecryptHeader(BREthereumFrameCoder fCoder, uint8_t * oBytes, size_t outSize) {
 
+    BREthereumFrameCoderContext* ctx = (BREthereumFrameCoderContext*) fCoder;
 
+    if(outSize != UINT256_SIZE) {
+        return ETHEREUM_BOOLEAN_FALSE;
+    }
+    
+    _updateMac(ctx, &ctx->ingressMac, oBytes, 16);
+    
+    UInt128 expected;
+    _ingressDigest(ctx,&expected);
+    
+    if(memcmp(&oBytes[outSize], expected.u8, 16)) {
+        return ETHEREUM_BOOLEAN_FALSE;
+    }
 
+    aes256_encrypt(&ctx->frameEncrypt, 16, oBytes, oBytes);
+    
+    return ETHEREUM_BOOLEAN_TRUE;
+}
+BREthereumBoolean ethereumFrameCoderDecryptFrame(BREthereumFrameCoder fCoder, uint8_t * oBytes, size_t outSize) {
+
+    BREthereumFrameCoderContext* ctx = (BREthereumFrameCoderContext*) fCoder;
+    size_t cipherLen = outSize - 16;
+    uint8_t cipher[cipherLen];
+    sha3_256_update(&ctx->ingressMac, cipherLen, cipher);
+    _updateMac(ctx, &ctx->ingressMac, NULL, 0);
+
+    UInt128 expected;
+    _ingressDigest(ctx,&expected);
+    
+    if(memcmp(&oBytes[cipherLen], expected.u8, 16)) {
+        return ETHEREUM_BOOLEAN_FALSE;
+    }
+    
+    aes256_encrypt(&ctx->frameEncrypt, outSize, oBytes, oBytes);
+    
+    return ETHEREUM_BOOLEAN_TRUE;
+}
--- ethereum/les/BREthereumFrameCoder.h
+++ ethereum/les/BREthereumFrameCoder.h
@@ -41,29 +41,50 @@ extern "C" {
  * Opaque pointer for a Frame coder
  */
 typedef struct BREthereumFrameCoderContext* BREthereumFrameCoder;
-
+    
 
 /**
- * Creates and initializes a packet framer
+ * Creates a frame coder
  */
-BREthereumFrameCoder ethereumFrameCoderCreate(UInt512* remoteEphemeral,
-                                              UInt256* remoteNonce,
-                                              BRKey* ecdheLocal,
-                                              UInt256* localNonce,
-                                              uint8_t* aukCipher,
-                                              size_t aukCipherLen,
-                                              uint8_t* authCiper,
-                                              size_t authCipherLen,
-                                              BREthereumBoolean didOriginate);
+extern BREthereumFrameCoder ethereumFrameCoderCreate(void);
     
+/**
+ * Initilaize a frame coder
+ */
+extern BREthereumBoolean ethereumFrameCoderInit(BREthereumFrameCoder fCoder,
+                                               UInt512* remoteEphemeral,
+                                               UInt256* remoteNonce,
+                                               BRKey* ecdheLocal,
+                                               UInt256* localNonce,
+                                               uint8_t* aukCipher,
+                                               size_t aukCipherLen,
+                                               uint8_t* authCiper,
+                                               size_t authCipherLen,
+                                               BREthereumBoolean didOriginate);
+    
+/**
+ * Frees the memory of the frame coder 
+ */
+extern void ethereumFrameCoderFree(BREthereumFrameCoder coder);
 
 /**
  * Writes a single frame to the coder
  */
- void ethereumFrameCoderWrite(BREthereumFrameCoder coder, uint8_t* payload, uint8_t* oFrame);
+ extern void ethereumFrameCoderWrite(BREthereumFrameCoder fCoder, uint8_t msgId,  uint8_t* payload, size_t payloadSize, uint8_t** oBytes, size_t * oBytesSize);
+
+/**
+ * Authenticates and descrptys the header 
+ */
+extern BREthereumBoolean ethereumFrameCoderDecryptHeader(BREthereumFrameCoder fCoder, uint8_t * oBytes, size_t outSize);
  
+/**
+ * Authenticates and descrptys the frame
+ */
+extern BREthereumBoolean ethereumFrameCoderDecryptFrame(BREthereumFrameCoder fCoder, uint8_t * oBytes, size_t outSize);
+  
  
 #ifdef __cplusplus
 }
 #endif
+
 #endif /* BR_Ethereum_Frame_Coder_h */
