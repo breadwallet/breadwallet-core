cd8d8da21b42b4e144dbc426492c3a6a905fc056
Ed Gamble <ed@breadwallet.com>
2018-12-11 09:28:34 -0800

    CORE-174: In BRPeerManagerDisconnect, set maxConnectCount to 0 thereby preventing spawning of new peer connections.

--- BRPeerManager.c
+++ BRPeerManager.c
@@ -1659,9 +1659,15 @@ void BRPeerManagerDisconnect(BRPeerManager *manager)
 {
     struct timespec ts;
     size_t peerCount, dnsThreadCount;
-    
+    int savedMaxConnectCount;
+
     assert(manager != NULL);
     pthread_mutex_lock(&manager->lock);
+
+    // Prevent new peers from being spawned
+    savedMaxConnectCount = manager->maxConnectCount;
+    manager->maxConnectCount = 0;
+
     peerCount = array_count(manager->connectedPeers);
     dnsThreadCount = manager->dnsThreadCount;
     
@@ -1681,6 +1687,10 @@ void BRPeerManagerDisconnect(BRPeerManager *manager)
         dnsThreadCount = manager->dnsThreadCount;
         pthread_mutex_unlock(&manager->lock);
     }
+
+    pthread_mutex_lock(&manager->lock);
+    manager->maxConnectCount = savedMaxConnectCount;
+    pthread_mutex_unlock(&manager->lock);
 }
 
 static int _BRPeerManagerRescan (BRPeerManager *manager, BRMerkleBlock *newLastBlock) {
