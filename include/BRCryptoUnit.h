3ba0a9818a385b43af41c2199d1fe8eb41873df5
Ed Gamble <ed@breadwallet.com>
2019-11-27 10:02:10 -0800

    CORE-751: Public header formatting.

--- include/BRCryptoUnit.h
+++ include/BRCryptoUnit.h
@@ -18,68 +18,68 @@
 extern "C" {
 #endif
 
-typedef struct BRCryptoUnitRecord *BRCryptoUnit;
+    typedef struct BRCryptoUnitRecord *BRCryptoUnit;
 
-private_extern BRCryptoUnit
-cryptoUnitCreateAsBase (BRCryptoCurrency currency,
-                        const char *uids,
-                        const char *name,
-                        const char *symbol);
+    private_extern BRCryptoUnit
+    cryptoUnitCreateAsBase (BRCryptoCurrency currency,
+                            const char *uids,
+                            const char *name,
+                            const char *symbol);
 
-private_extern BRCryptoUnit
-cryptoUnitCreate (BRCryptoCurrency currency,
-                  const char *uids,
-                  const char *name,
-                  const char *symbol,
-                  BRCryptoUnit baseUnit,
-                  uint8_t powerOffset);
+    private_extern BRCryptoUnit
+    cryptoUnitCreate (BRCryptoCurrency currency,
+                      const char *uids,
+                      const char *name,
+                      const char *symbol,
+                      BRCryptoUnit baseUnit,
+                      uint8_t powerOffset);
 
 
-extern const char *
-cryptoUnitGetUids(BRCryptoUnit unit);
+    extern const char *
+    cryptoUnitGetUids(BRCryptoUnit unit);
 
-extern const char *
-cryptoUnitGetName (BRCryptoUnit unit);
+    extern const char *
+    cryptoUnitGetName (BRCryptoUnit unit);
 
-extern const char *
-cryptoUnitGetSymbol (BRCryptoUnit unit);
+    extern const char *
+    cryptoUnitGetSymbol (BRCryptoUnit unit);
 
-/**
- * Returns the unit's currency
- *
- * @param unit the Unit
- *
- * @return The currency w/ an incremented reference count (aka 'taken')
- */
-extern BRCryptoCurrency
-cryptoUnitGetCurrency (BRCryptoUnit unit);
+    /**
+     * Returns the unit's currency
+     *
+     * @param unit the Unit
+     *
+     * @return The currency w/ an incremented reference count (aka 'taken')
+     */
+    extern BRCryptoCurrency
+    cryptoUnitGetCurrency (BRCryptoUnit unit);
 
-extern BRCryptoBoolean
-cryptoUnitHasCurrency (BRCryptoUnit unit,
-                       BRCryptoCurrency currency);
+    extern BRCryptoBoolean
+    cryptoUnitHasCurrency (BRCryptoUnit unit,
+                           BRCryptoCurrency currency);
 
-/**
- * Returns the unit's base unit.  If unit is itself the base unit then unit is returned
- *
- * @param unit The unit
- *
- * @return the base unit w/ an incremented reference count (aka 'taken')
- */
-extern BRCryptoUnit
-cryptoUnitGetBaseUnit (BRCryptoUnit unit);
+    /**
+     * Returns the unit's base unit.  If unit is itself the base unit then unit is returned
+     *
+     * @param unit The unit
+     *
+     * @return the base unit w/ an incremented reference count (aka 'taken')
+     */
+    extern BRCryptoUnit
+    cryptoUnitGetBaseUnit (BRCryptoUnit unit);
 
-extern uint8_t
-cryptoUnitGetBaseDecimalOffset (BRCryptoUnit unit);
+    extern uint8_t
+    cryptoUnitGetBaseDecimalOffset (BRCryptoUnit unit);
 
-extern BRCryptoBoolean
-cryptoUnitIsCompatible (BRCryptoUnit u1,
-                        BRCryptoUnit u2);
+    extern BRCryptoBoolean
+    cryptoUnitIsCompatible (BRCryptoUnit u1,
+                            BRCryptoUnit u2);
 
-extern BRCryptoBoolean
-cryptoUnitIsIdentical (BRCryptoUnit u1,
-                       BRCryptoUnit u2);
+    extern BRCryptoBoolean
+    cryptoUnitIsIdentical (BRCryptoUnit u1,
+                           BRCryptoUnit u2);
 
-DECLARE_CRYPTO_GIVE_TAKE (BRCryptoUnit, cryptoUnit);
+    DECLARE_CRYPTO_GIVE_TAKE (BRCryptoUnit, cryptoUnit);
 
 #ifdef __cplusplus
 }
