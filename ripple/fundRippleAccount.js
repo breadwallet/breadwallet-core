62c44f80162eeaade3996be08dc46fb6b1e313f0
Carl <carlcherry@gmail.com>
2020-01-09 15:39:12 -0800

    CORE-795: Fixed missing variable (address)

--- ripple/fundRippleAccount.js
+++ ripple/fundRippleAccount.js
@@ -4,13 +4,14 @@ const RippleAPI = require('ripple-lib').RippleAPI; // require('ripple-lib')
 // To fund a test account
 // 1. create a new test account on Ripple testnet - this account
 //    will be used to fund your wallet account on ripple-testnet
-//    copy the address and secret below const address = '';
+//    copy the address and secret below
 // 2. In your wallet - find the the ripple address for your test
 //    account and copy it into the destination address below
 // 3. The test account above only has a max of 1000 XRP, set the
 //    amount for the destination to whaterver you want. If your test
 //    account needs more than 900 XRP then you will have to repeat step
 //    1 and get another funding account
+const address = '';
 const secret = '';
 
 const api = new RippleAPI({server: 'wss://s.altnet.rippletest.net:51233'});
