cf5b3749b17f73ce8699152bdcd2141f9eada2f0
Ed Gamble <ed@breadwallet.com>
2020-01-13 09:36:44 -0800

    CORE-796: Update Network 'Installed Builtins' Parameters.

--- crypto/BRCryptoConfig.h
+++ crypto/BRCryptoConfig.h
@@ -35,16 +35,16 @@
 // MARK: - BTC
 
 #define NETWORK_NAME    "Bitcoin"
-DEFINE_NETWORK (btcMainnet,  "bitcoin-mainnet", NETWORK_NAME, "mainnet", true, 595000, 6)
-DEFINE_NETWORK_FEE_ESTIMATE ("bitcoin-mainnet", "30", "10m", 10 * 60 * 1000)
+DEFINE_NETWORK (btcMainnet,  "bitcoin-mainnet", NETWORK_NAME, "mainnet", true, 612691, 6)
+DEFINE_NETWORK_FEE_ESTIMATE ("bitcoin-mainnet", "18", "10m", 10 * 60 * 1000)
 DEFINE_CURRENCY ("bitcoin-mainnet",     "bitcoin-mainnet:__native__",   NETWORK_NAME,  "btc",  "native",   NULL,   true)
     DEFINE_UNIT ("bitcoin-mainnet:__native__",      "Satoshi",    "sat",      0,      "SAT")
     DEFINE_UNIT ("bitcoin-mainnet:__native__",      NETWORK_NAME, "btc",      8,      "₿")
 DEFINE_ADDRESS_SCHEMES  ("bitcoin-mainnet", CRYPTO_ADDRESS_SCHEME_BTC_SEGWIT,   CRYPTO_ADDRESS_SCHEME_BTC_LEGACY)
 DEFINE_MODES            ("bitcoin-mainnet", CRYPTO_SYNC_MODE_API_ONLY,          CRYPTO_SYNC_MODE_P2P_ONLY)
 
-DEFINE_NETWORK (btcTestnet,  "bitcoin-testnet", NETWORK_NAME, "testnet", false, 1575000, 6)
-DEFINE_NETWORK_FEE_ESTIMATE ("bitcoin-testnet", "30", "10m", 10 * 60 * 1000)
+DEFINE_NETWORK (btcTestnet,  "bitcoin-testnet", NETWORK_NAME, "testnet", false, 1660401, 6)
+DEFINE_NETWORK_FEE_ESTIMATE ("bitcoin-testnet", "18", "10m", 10 * 60 * 1000)
 DEFINE_CURRENCY ("bitcoin-testnet",     "bitcoin-testnet:__native__",   NETWORK_NAME,  "btc",  "native",   NULL,   true)
     DEFINE_UNIT ("bitcoin-testnet:__native__",      "Satoshi",    "sat",      0,      "SAT")
     DEFINE_UNIT ("bitcoin-testnet:__native__",      NETWORK_NAME, "btc",      8,      "₿")
@@ -55,16 +55,16 @@ DEFINE_MODES            ("bitcoin-testnet", CRYPTO_SYNC_MODE_API_ONLY,          
 // MARK: - BCH
 
 #define NETWORK_NAME    "Bitcoin Cash"
-DEFINE_NETWORK (bchMainnet,  "bitcoincash-mainnet", NETWORK_NAME, "mainnet", true, 600000, 6)
-DEFINE_NETWORK_FEE_ESTIMATE ("bitcoincash-mainnet", "30", "10m", 10 * 60 * 1000)
+DEFINE_NETWORK (bchMainnet,  "bitcoincash-mainnet", NETWORK_NAME, "mainnet", true, 617640, 6)
+DEFINE_NETWORK_FEE_ESTIMATE ("bitcoincash-mainnet", "2", "10m", 20 * 60 * 1000)
 DEFINE_CURRENCY ("bitcoincash-mainnet",     "bitcoincash-mainnet:__native__",   NETWORK_NAME,  "bch",  "native",   NULL,   true)
     DEFINE_UNIT ("bitcoincash-mainnet:__native__",      "Satoshi",    "sat",      0,      "SAT")
     DEFINE_UNIT ("bitcoincash-mainnet:__native__",      NETWORK_NAME, "bch",      8,      "BCH")
 DEFINE_ADDRESS_SCHEMES  ("bitcoincash-mainnet", CRYPTO_ADDRESS_SCHEME_BTC_LEGACY)
 DEFINE_MODES            ("bitcoincash-mainnet", CRYPTO_SYNC_MODE_API_ONLY, CRYPTO_SYNC_MODE_P2P_ONLY)
 
-DEFINE_NETWORK (bchTestnet,  "bitcoincash-testnet", NETWORK_NAME, "testnet", false, 1325000, 6)
-DEFINE_NETWORK_FEE_ESTIMATE ("bitcoincash-testnet", "30", "10m", 10 * 60 * 1000)
+DEFINE_NETWORK (bchTestnet,  "bitcoincash-testnet", NETWORK_NAME, "testnet", false, 1353306, 6)
+DEFINE_NETWORK_FEE_ESTIMATE ("bitcoincash-testnet", "2", "10m", 20 * 60 * 1000)
 DEFINE_CURRENCY ("bitcoincash-testnet",     "bitcoincash-testnet:__native__",   NETWORK_NAME,  "bch",  "native",   NULL,   true)
     DEFINE_UNIT ("bitcoincash-testnet:__native__",      "Satoshi",    "sat",      0,      "SAT")
     DEFINE_UNIT ("bitcoincash-testnet:__native__",      NETWORK_NAME, "bch",      8,      "BCH")
@@ -75,8 +75,8 @@ DEFINE_MODES            ("bitcoincash-testnet", CRYPTO_SYNC_MODE_API_ONLY,  CRYP
 // MARK: - ETH
 
 #define NETWORK_NAME    "Ethereum"
-DEFINE_NETWORK (ethMainnet,  "ethereum-mainnet", NETWORK_NAME, "mainnet", true, 8570000, 6)
-DEFINE_NETWORK_FEE_ESTIMATE ("ethereum-mainnet", "2000000000", "1m", 1 * 60 * 1000)
+DEFINE_NETWORK (ethMainnet,  "ethereum-mainnet", NETWORK_NAME, "mainnet", true, 9273958, 6)
+DEFINE_NETWORK_FEE_ESTIMATE ("ethereum-mainnet", "25000000000", "1m", 1 * 60 * 1000)
 DEFINE_CURRENCY ("ethereum-mainnet",     "ethereum-mainnet:__native__",   NETWORK_NAME,  "eth",  "native",   NULL,   true)
     DEFINE_UNIT ("ethereum-mainnet:__native__",      "Wei",         "wei",      0,      "WEI")
     DEFINE_UNIT ("ethereum-mainnet:__native__",      "Gwei",        "gwei",     9,      "GWEI")
@@ -87,8 +87,8 @@ DEFINE_CURRENCY ("ethereum-mainnet",    "ethereum-mainnet:0x558ec3152e2eb2174905
 DEFINE_ADDRESS_SCHEMES  ("ethereum-mainnet", CRYPTO_ADDRESS_SCHEME_ETH_DEFAULT)
 DEFINE_MODES            ("ethereum-mainnet", CRYPTO_SYNC_MODE_API_ONLY, CRYPTO_SYNC_MODE_API_WITH_P2P_SEND, CRYPTO_SYNC_MODE_P2P_ONLY)
 
-DEFINE_NETWORK (ethRopsten,  "ethereum-ropsten", NETWORK_NAME, "testnet", false, 6415000, 6)
-DEFINE_NETWORK_FEE_ESTIMATE ("ethereum-ropsten", "2000000000", "1m", 1 * 60 * 1000)
+DEFINE_NETWORK (ethRopsten,  "ethereum-ropsten", NETWORK_NAME, "testnet", false, 7119019, 6)
+DEFINE_NETWORK_FEE_ESTIMATE ("ethereum-ropsten", "17500000000", "1m", 1 * 60 * 1000)
 DEFINE_CURRENCY ("ethereum-ropsten",     "ethereum-ropsten:__native__",   NETWORK_NAME,  "eth",  "native",   NULL,   true)
     DEFINE_UNIT ("ethereum-ropsten:__native__",      "Wei",         "wei",      0,      "WEI")
     DEFINE_UNIT ("ethereum-ropsten:__native__",      "Gwei",        "gwei",     9,      "GWEI")
@@ -103,7 +103,7 @@ DEFINE_MODES            ("ethereum-ropsten", CRYPTO_SYNC_MODE_API_ONLY, CRYPTO_S
 // MARK: XRP
 
 #define NETWORK_NAME    "Ripple"
-DEFINE_NETWORK (xrpMainnet,  "ripple-mainnet", NETWORK_NAME, "mainnet", true, 52473542, 6)
+DEFINE_NETWORK (xrpMainnet,  "ripple-mainnet", NETWORK_NAME, "mainnet", true, 52713481, 6)
 DEFINE_NETWORK_FEE_ESTIMATE ("ripple-mainnet", "10", "1m", 1 * 60 * 1000)
 DEFINE_CURRENCY ("ripple-mainnet",     "ripple-mainnet:__native__",   NETWORK_NAME,  "xrp",  "native",   NULL,   true)
     DEFINE_UNIT ("ripple-mainnet:__native__",      "Drop",       "drop",      0,      "DROP")
@@ -111,7 +111,7 @@ DEFINE_CURRENCY ("ripple-mainnet",     "ripple-mainnet:__native__",   NETWORK_NA
 DEFINE_ADDRESS_SCHEMES  ("ripple-mainnet", CRYPTO_ADDRESS_SCHEME_GEN_DEFAULT)
 DEFINE_MODES            ("ripple-mainnet", CRYPTO_SYNC_MODE_API_ONLY)
 
-DEFINE_NETWORK (xrpTestnet,  "ripple-testnet", NETWORK_NAME, "testnet", false, 52473542, 6)
+DEFINE_NETWORK (xrpTestnet,  "ripple-testnet", NETWORK_NAME, "testnet", false, 0, 6)
 DEFINE_NETWORK_FEE_ESTIMATE ("ripple-testnet", "10", "1m", 1 * 60 * 1000)
 DEFINE_CURRENCY ("ripple-testnet",     "ripple-testnet:__native__",   NETWORK_NAME,  "xrp",  "native",   NULL,   true)
     DEFINE_UNIT ("ripple-testnet:__native__",      "Drop",       "drop",      0,      "DROP")
